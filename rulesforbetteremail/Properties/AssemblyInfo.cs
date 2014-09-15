09afb54b50386add8e4e43110d2b550009bd5991
Robert MacLean <robert@sadev.co.za>
2014-09-15 22:05:27 +0200

    v1

---
+++ rulesforbetteremail/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("rulesforbetteremail")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("rulesforbetteremail")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("2534a105-94a2-4b7f-9e18-c34ca219c150")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ rulesforbetteremail/Scripts/select.js
@@ -0,0 +1,23 @@
+﻿/// <reference path="jquery-2.1.1.js" />
+
+$(function () {
+    $("#sig").click(function () {
+        var signatureBlock = $(this);
+        signatureBlock.focus();
+        signatureBlock.select();
+    });
+});
+
+
+(function (i, s, o, g, r, a, m) {
+    i['GoogleAnalyticsObject'] = r; i[r] = i[r] || function () {
+        (i[r].q = i[r].q || []).push(arguments)
+    }, i[r].l = 1 * new Date(); a = s.createElement(o),
+    m = s.getElementsByTagName(o)[0]; a.async = 1; a.src = g; m.parentNode.insertBefore(a, m)
+})(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+
+ga('create', 'UA-2477039-8', 'auto');
+ga('send', 'pageview');
+
+var switchTo5x=true;
+stLight.options({publisher: "6e331d38-b4e0-406c-904a-be8407614a4d", doNotHash: false, doNotCopy: false, hashAddressBar: false});
